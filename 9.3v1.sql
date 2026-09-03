3eb522256fc5d48e9993b718f617eb8d1520292a
Charmmy-ing <lc5207242@gmail.com>
2026-09-03 15:28:38 +0800

    9/3v1

---
+++ 9.3v1.sql
@@ -0,0 +1,19 @@
+/*
+ 存储引擎
+ */
+  show create table user;
+show engines;
+create  table my_myisam
+(
+    id int
+
+) engine=MyISAM;
+create table memory(
+    name varchar(10)
+) engine=memory;
+show create table memory;
+create table InnoDB(
+    id int,
+    name varchar(10)
+) engine=innodb;
+show create table innodb;
